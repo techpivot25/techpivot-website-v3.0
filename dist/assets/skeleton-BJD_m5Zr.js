c5c5fd0aecd1650c94322c5f492b5f479e928e4c
Techpivot25 <techpivot25@gmail.com>
2026-09-17 21:56:59 +0530

    new commit

---
+++ dist/assets/skeleton-BJD_m5Zr.js
@@ -0,0 +1,1 @@
+import{a as e}from"./animation-vendor-DZMEM02K.js";import{d as t}from"./index-BBwhuYXF.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{className:t(`animate-pulse rounded-md bg-muted`,e),...r})}export{r as t};
